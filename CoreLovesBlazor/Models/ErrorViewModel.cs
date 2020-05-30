10699f571f4ebfa523a765ebb2463a4b9cdec9e6
sen <gupta.sen@experiom.in>
2020-05-30 14:48:28 +0530

    Add project files.

---
+++ CoreLovesBlazor/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreLovesBlazor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
